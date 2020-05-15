b6221cba88e9816092019880c770a460549bee18
Ondrej Strnad <ondrej.strnad@kaust.edu.sa>
2020-05-15 15:10:34 +0300

    enable/disable of the slider

--- src/js/dialogs/EnvmapLoadDialog.js
+++ src/js/dialogs/EnvmapLoadDialog.js
@@ -6,132 +6,143 @@
 
 class EnvmapLoadDialog extends AbstractDialog {
 
-constructor(options) {
-    super(UISPECS.EnvmapLoadDialog, options);
+    constructor(options) {
+        super(UISPECS.EnvmapLoadDialog, options);
 
-    this._handleTypeChange = this._handleTypeChange.bind(this);
-    this._handleLoadClick = this._handleLoadClick.bind(this);
-    this._handleFileChange = this._handleFileChange.bind(this);
-    this._handleURLChange = this._handleURLChange.bind(this);
-    this._handleDemoChange = this._handleDemoChange.bind(this);
+        this._handleTypeChange = this._handleTypeChange.bind(this);
+        this._handleLoadClick = this._handleLoadClick.bind(this);
+        this._handleFileChange = this._handleFileChange.bind(this);
+        this._handleURLChange = this._handleURLChange.bind(this);
+        this._handleDemoChange = this._handleDemoChange.bind(this);
+        this._handleEnabledChange = this._handleEnabledChange.bind(this);
 
-    this._demos = [];
+        this._demos = [];
 
-    this._addEventListeners();
-    this._loadDemoJson();
-}
+        this._addEventListeners();
+        this._loadDemoJson();
+    }
 
-_addEventListeners() {
-    this._binds.type.addEventListener('change', this._handleTypeChange);
-    this._binds.loadButton.addEventListener('click', this._handleLoadClick);
-    this._binds.file.addEventListener('change', this._handleFileChange);
-    this._binds.url.addEventListener('input', this._handleURLChange);
-    this._binds.demo.addEventListener('change', this._handleDemoChange);
-}
+    _addEventListeners() {
+        this._binds.type.addEventListener('change', this._handleTypeChange);
+        this._binds.loadButton.addEventListener('click', this._handleLoadClick);
+        this._binds.file.addEventListener('change', this._handleFileChange);
+        this._binds.url.addEventListener('input', this._handleURLChange);
+        this._binds.demo.addEventListener('change', this._handleDemoChange);
 
-_loadDemoJson() {
-    // TODO: rewrite with fetch
-    const xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', () => {
-        if (xhr.status === 200) {
-            this._demos = JSON.parse(xhr.responseText);
-            this._demos.forEach(demo => {
-                this._binds.demo.addOption(demo.value, demo.label);
-            });
+        this._binds.enabledCheckbox.addEventListener('change', this._handleEnabledChange);
+    }
+
+    _loadDemoJson() {
+        // TODO: rewrite with fetch
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', () => {
+            if (xhr.status === 200) {
+                this._demos = JSON.parse(xhr.responseText);
+                this._demos.forEach(demo => {
+                    this._binds.demo.addOption(demo.value, demo.label);
+                });
+            }
+        });
+        xhr.open('GET', 'demo-envmaps.json');
+        xhr.send();
+    }
+
+    _handleLoadClick() {
+        switch (this._binds.type.getValue()) {
+            case 'file': this._handleLoadFile(); break;
+            case 'url': this._handleLoadURL(); break;
+            case 'demo': this._handleLoadDemo(); break;
         }
-    });
-    xhr.open('GET', 'demo-envmaps.json');
-    xhr.send();
-}
-
-_handleLoadClick() {
-    switch (this._binds.type.getValue()) {
-        case 'file' : this._handleLoadFile(); break;
-        case 'url'  : this._handleLoadURL();  break;
-        case 'demo' : this._handleLoadDemo(); break;
     }
-}
 
-_handleLoadFile() {
-    const files = this._binds.file.getFiles();
-    if (files.length === 0) {
-        // update status bar?
-        return;
+    _handleLoadFile() {
+        const files = this._binds.file.getFiles();
+        if (files.length === 0) {
+            // update status bar?
+            return;
+        }
+        const file = files[0];
+
+        this.trigger('load', {
+            type: 'file',
+            file: file
+        });
     }
-    const file = files[0];
 
-    this.trigger('load', {
-        type : 'file',
-        file : file
-    });
-}
-
-_handleLoadURL() {
-    const url = this._binds.url.getValue();
-    this.trigger('load', {
-        type : 'url',
-        url  : url
-    });
-}
-
-_handleLoadDemo() {
-    const demo = this._binds.demo.getValue();
-    const found = this._demos.find(d => d.value === demo);
-    this.trigger('load', {
-        type : 'url',
-        url  : found.url
-    });
-}
-
-_handleTypeChange() {
-    // TODO: switching _panel
-    switch (this._binds.type.getValue()) {
-        case 'file':
-            this._binds.filePanel.show();
-            this._binds.urlPanel.hide();
-            this._binds.demoPanel.hide();
-            break;
-        case 'url':
-            this._binds.filePanel.hide();
-            this._binds.urlPanel.show();
-            this._binds.demoPanel.hide();
-            break;
-        case 'demo':
-            this._binds.filePanel.hide();
-            this._binds.urlPanel.hide();
-            this._binds.demoPanel.show();
-            break;
+    _handleLoadURL() {
+        const url = this._binds.url.getValue();
+        this.trigger('load', {
+            type: 'url',
+            url: url
+        });
     }
-    this._updateLoadButtonAndProgressVisibility();
-}
 
-_handleFileChange() {
-    this._updateLoadButtonAndProgressVisibility();
-}
-
-_handleURLChange() {
-    this._updateLoadButtonAndProgressVisibility();
-}
-
-_handleDemoChange() {
-    this._updateLoadButtonAndProgressVisibility();
-}
-
-_updateLoadButtonAndProgressVisibility() {
-    switch (this._binds.type.getValue()) {
-        case 'file':
-            const files = this._binds.file.getFiles();
-            this._binds.loadButtonAndProgress.setVisible(files.length > 0);
-            break;
-        case 'url':
-            const urlEmpty = this._binds.url.getValue() === '';
-            this._binds.loadButtonAndProgress.setVisible(!urlEmpty);
-            break;
-        case 'demo':
-            const demo = this._binds.demo.getValue();
-            this._binds.loadButtonAndProgress.setVisible(!!demo);
-            break;
+    _handleLoadDemo() {
+        const demo = this._binds.demo.getValue();
+        const found = this._demos.find(d => d.value === demo);
+        this.trigger('load', {
+            type: 'url',
+            url: found.url
+        });
     }
-}
+
+    _handleTypeChange() {
+        // TODO: switching _panel
+        switch (this._binds.type.getValue()) {
+            case 'file':
+                this._binds.filePanel.show();
+                this._binds.urlPanel.hide();
+                this._binds.demoPanel.hide();
+                break;
+            case 'url':
+                this._binds.filePanel.hide();
+                this._binds.urlPanel.show();
+                this._binds.demoPanel.hide();
+                break;
+            case 'demo':
+                this._binds.filePanel.hide();
+                this._binds.urlPanel.hide();
+                this._binds.demoPanel.show();
+                break;
+        }
+        this._updateLoadButtonAndProgressVisibility();
+    }
+
+    _handleFileChange() {
+        this._updateLoadButtonAndProgressVisibility();
+    }
+
+    _handleURLChange() {
+        this._updateLoadButtonAndProgressVisibility();
+    }
+
+    _handleDemoChange() {
+        this._updateLoadButtonAndProgressVisibility();
+    }
+
+    _updateLoadButtonAndProgressVisibility() {
+        switch (this._binds.type.getValue()) {
+            case 'file':
+                const files = this._binds.file.getFiles();
+                this._binds.loadButtonAndProgress.setVisible(files.length > 0);
+                break;
+            case 'url':
+                const urlEmpty = this._binds.url.getValue() === '';
+                this._binds.loadButtonAndProgress.setVisible(!urlEmpty);
+                break;
+            case 'demo':
+                const demo = this._binds.demo.getValue();
+                this._binds.loadButtonAndProgress.setVisible(!!demo);
+                break;
+        }
+    }
+
+    _handleEnabledChange() {           
+        if(this._binds.enabledCheckbox.isChecked()) {
+            this._binds.multiTrackSlider.enable();
+        } else {
+            this._binds.multiTrackSlider.disable();
+        }
+    }
 
 }
--- src/js/ui/SliderMultiTrack.js
+++ src/js/ui/SliderMultiTrack.js
@@ -5,134 +5,159 @@
 
 class SliderMultiTrack extends UIObject {
 
-constructor(options) {
-    super(TEMPLATES.SliderMultiTrack, options);
+    constructor(options) {
+        super(TEMPLATES.SliderMultiTrack, options);
 
-    Object.assign(this, {
-        value       : 0,
-        value2      : 0,
-        value3      : 0,
-        min         : 0,
-        max         : 100,
-        step        : 1,
-        disabled     :false,
-        logarithmic : false
-    }, options);
+        Object.assign(this, {
+            value: 0,
+            value2: 0,
+            value3: 0,
+            min: 0,
+            max: 100,
+            step: 1,
+            enabled: true,
+            logarithmic: false
+        }, options);
 
-    this._handleMouseDown = this._handleMouseDown.bind(this);
-    this._handleMouseUp   = this._handleMouseUp.bind(this);
-    this._handleMouseMove = this._handleMouseMove.bind(this);
-    this._handleWheel     = this._handleWheel.bind(this);
-   
-    this._updateUI();
+        this._handleMouseDown = this._handleMouseDown.bind(this);
+        this._handleMouseUp = this._handleMouseUp.bind(this);
+        this._handleMouseMove = this._handleMouseMove.bind(this);
+        this._handleWheel = this._handleWheel.bind(this);
 
-    this._element.addEventListener('mousedown', this._handleMouseDown);
-    this._element.addEventListener('wheel', this._handleWheel);
-}
-isDisable()
-{
-    return this.disabled;
-}
-disable()
-{
-    this.disabled=true;
-    /* */
-}
-enable()
-{
-    this.disabled=false;
-    /*  */
-}
-destroy() {
-    document.removeEventListener('mouseup', this._handleMouseUp);
-    document.removeEventListener('mousemove', this._handleMouseMove);
+        this._updateUI();
 
-    super.destroy();
-}
+        this._element.addEventListener('mousedown', this._handleMouseDown);
+        this._element.addEventListener('wheel', this._handleWheel);
 
-setValue(value) {
-    this.value = CommonUtils.clamp(value, this.min, this.max);
-    this._updateUI();
-    this.trigger('change');
-}
-
-setValue2(value) {
-    this.value2 = CommonUtils.clamp(value, this.min, this.max);
-    this._updateUI();
-}
-
-setValue3(value) {
-    this.value3 = CommonUtils.clamp(value, this.min, this.max);
-    this._updateUI();
-}
-getMaxValue()
-{
-    return this.max;
-}
-_updateUI() {
-    if (this.logarithmic) {
-        const logmin = Math.log(this.min);
-        const logmax = Math.log(this.max);
-        const ratio = (Math.log(this.value) - logmin) / (logmax - logmin) * 100;
-        this._binds.button.style.marginLeft = ratio + '%';
-    } else {
-        const ratio = (this.value - this.min) / (this.max - this.min) * 100;
-        this._binds.button.style.marginLeft = ratio + '%';
-        
-        const ratio2 = (this.value2 - this.min) / (this.max - this.min) * 100;
-        this._binds.track2.style.width = ratio2 + '%';
-
-        const ratio3 = (this.value3 - this.min) / (this.max - this.min) * 100;
-        this._binds.track3.style.width = ratio3 + '%';
-    }
-}
-
-getValue() {
-    return this.value;
-}
-
-_setValueByEvent(e) {
-    const rect = this._binds._container.getBoundingClientRect();
-    const ratio = (e.pageX - rect.left) / (rect.right - rect.left);
-    if (this.logarithmic) {
-        const logmin = Math.log(this.min);
-        const logmax = Math.log(this.max);
-        const value = Math.exp(logmin + ratio * (logmax - logmin));
-        this.setValue(value);
-    } else {
-        const value = this.min + ratio * (this.max - this.min);
-        this.setValue(value);        
-    }
-}
-
-_handleMouseDown(e) {
-    document.addEventListener('mouseup', this._handleMouseUp);
-    document.addEventListener('mousemove', this._handleMouseMove);
-    this._setValueByEvent(e);
-}
-
-_handleMouseUp(e) {
-    document.removeEventListener('mouseup', this._handleMouseUp);
-    document.removeEventListener('mousemove', this._handleMouseMove);
-    this._setValueByEvent(e);
-}
-
-_handleMouseMove(e) {
-    this._setValueByEvent(e);
-}
-
-_handleWheel(e) {
-    let wheel = e.deltaY;
-    if (wheel < 0) {
-        wheel = 1;
-    } else if (wheel > 0) {
-        wheel = -1;
-    } else {
-        wheel = 0;
+        this.enable();        
     }
 
-    const delta = this.logarithmic ? this.value * this.step * wheel : this.step * wheel;
-    this.setValue(this.value + delta);
-}
+    isDisable() {
+        return !this.enabled;
+    }
+
+    disable() {
+        this.enabled = false;
+        this._binds.button.classList.toggle('disabled', true);        
+    }
+
+    enable() {
+        this.enabled = true;
+        this._binds.button.classList.toggle('disabled', false);        
+    }
+
+    destroy() {
+        document.removeEventListener('mouseup', this._handleMouseUp);
+        document.removeEventListener('mousemove', this._handleMouseMove);
+
+        super.destroy();
+    }
+
+    setValue(value) {
+        if (!this.enabled) {
+            return;
+        }
+
+        this.value = CommonUtils.clamp(value, this.min, this.max);
+        this._updateUI();
+        this.trigger('change');
+    }
+
+    setValue2(value) {
+        // there is no 'disabled' test because it's just state visualization
+        this.value2 = CommonUtils.clamp(value, this.min, this.max);
+        this._updateUI();
+    }
+
+    setValue3(value) {
+        // there is no 'disabled' test because it's just state visualization
+        this.value3 = CommonUtils.clamp(value, this.min, this.max);
+        this._updateUI();
+    }
+
+    getMaxValue() {
+        return this.max;
+    }
+
+    _updateUI() {
+        if (this.logarithmic) {
+            const logmin = Math.log(this.min);
+            const logmax = Math.log(this.max);
+            const ratio = (Math.log(this.value) - logmin) / (logmax - logmin) * 100;
+            this._binds.button.style.marginLeft = ratio + '%';
+        } else {
+            const ratio = (this.value - this.min) / (this.max - this.min) * 100;
+            this._binds.button.style.marginLeft = ratio + '%';
+
+            const ratio2 = (this.value2 - this.min) / (this.max - this.min) * 100;
+            this._binds.track2.style.width = ratio2 + '%';
+
+            const ratio3 = (this.value3 - this.min) / (this.max - this.min) * 100;
+            this._binds.track3.style.width = ratio3 + '%';
+        }
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    _setValueByEvent(e) {
+        const rect = this._binds._container.getBoundingClientRect();
+        const ratio = (e.pageX - rect.left) / (rect.right - rect.left);
+        if (this.logarithmic) {
+            const logmin = Math.log(this.min);
+            const logmax = Math.log(this.max);
+            const value = Math.exp(logmin + ratio * (logmax - logmin));
+            this.setValue(value);
+        } else {
+            const value = this.min + ratio * (this.max - this.min);
+            this.setValue(value);
+        }
+    }
+
+    _handleMouseDown(e) {        
+        if (!this.enabled) {
+            return;
+        }        
+        document.addEventListener('mouseup', this._handleMouseUp);
+        document.addEventListener('mousemove', this._handleMouseMove);
+        this._setValueByEvent(e);
+    }
+
+    _handleMouseUp(e) {
+        if (!this.enabled) {
+            return;
+        }
+
+        document.removeEventListener('mouseup', this._handleMouseUp);
+        document.removeEventListener('mousemove', this._handleMouseMove);
+        this._setValueByEvent(e);
+    }
+
+    _handleMouseMove(e) {
+        if (!this.enabled) {
+            return;
+        }
+
+        this._setValueByEvent(e);
+    }
+
+    _handleWheel(e) {
+        if (!this.enabled) {
+            return;
+        }
+
+        let wheel = e.deltaY;
+        if (wheel < 0) {
+            wheel = 1;
+        } else if (wheel > 0) {
+            wheel = -1;
+        } else {
+            wheel = 0;
+        }
+
+        const delta = this.logarithmic ? this.value * this.step * wheel : this.step * wheel;
+        this.setValue(this.value + delta);
+    }
 
 }
