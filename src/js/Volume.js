c2d3b8faea100dbdbe6c17360a4e79edbe689a1a
Žiga Lesar <terier.kuza@gmail.com>
2020-06-09 00:10:00 +0200

    Read data type from BVP modality

--- src/js/Volume.js
+++ src/js/Volume.js
@@ -77,11 +77,12 @@ readModality(modalityName, handlers) {
                 const position = placement.position;
                 const block = blocks[placement.index];
                 const blockdim = block.dimensions;
+                const typedData = this._typize(data, modality.type);
                 gl.bindTexture(gl.TEXTURE_3D, this._texture);
                 gl.texSubImage3D(gl.TEXTURE_3D, 0,
                     position.x, position.y, position.z,
                     blockdim.width, blockdim.height, blockdim.depth,
-                    modality.format, gl.UNSIGNED_INT, new Uint32Array(data));
+                    modality.format, modality.type, typedData);
                 remainingBlocks--;
                 if (remainingBlocks === 0) {
                     this.ready = true;
@@ -90,6 +91,29 @@ readModality(modalityName, handlers) {
             }
         });
     });
+}
+
+_typize(data, type) {
+    const gl = this._gl;
+    switch (type) {
+        case gl.BYTE:                         return new Int8Array(data);
+        case gl.UNSIGNED_BYTE:                return new Uint8Array(data);
+        case gl.UNSIGNED_BYTE:                return new Uint8ClampedArray(data);
+        case gl.SHORT:                        return new Int16Array(data);
+        case gl.UNSIGNED_SHORT:               return new Uint16Array(data);
+        case gl.UNSIGNED_SHORT_5_6_5:         return new Uint16Array(data);
+        case gl.UNSIGNED_SHORT_5_5_5_1:       return new Uint16Array(data);
+        case gl.UNSIGNED_SHORT_4_4_4_4:       return new Uint16Array(data);
+        case gl.INT:                          return new Int32Array(data);
+        case gl.UNSIGNED_INT:                 return new Uint32Array(data);
+        case gl.UNSIGNED_INT_5_9_9_9_REV:     return new Uint32Array(data);
+        case gl.UNSIGNED_INT_2_10_10_10_REV:  return new Uint32Array(data);
+        case gl.UNSIGNED_INT_10F_11F_11F_REV: return new Uint32Array(data);
+        case gl.UNSIGNED_INT_24_8:            return new Uint32Array(data);
+        case gl.HALF_FLOAT:                   return new Uint16Array(data);
+        case gl.FLOAT:                        return new Float32Array(data);
+        default: throw new Error('Unknown volume datatype: ' + type);
+    }
 }
 
 getTexture() {
