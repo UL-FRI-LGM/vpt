9fb6513ea6950b63a9a60512d863fe2f3bcc2afe
ondrejstrnad <ondrejstrnad@gmail.com>
2020-08-14 10:00:35 +0300

    WIP: camera control button

--- src/js/Camera.js
+++ src/js/Camera.js
@@ -19,6 +19,8 @@ class Camera {
         this.projectionMatrix = new Matrix();
         this.transformationMatrix = new Matrix();
         this.isDirty = false;
+
+        this.createOnScreenHandles();
     }
 
     updateViewMatrix() {
@@ -62,4 +64,87 @@ class Camera {
         this.isDirty = true;
     }
 
+    createOnScreenHandles() {
+        var wrapper = document.createElement("div");
+        wrapper.className = "camera-controls-wrapper";
+        document.body.appendChild(wrapper);
+
+        var _this = this;
+
+        // zoom-in
+        var btn = this.addButton(wrapper, 'camera-zoom-in');
+        btn.onclick = function() {
+            _this.zoom(10);
+        };
+        // zoom-out
+        var btn = this.addButton(wrapper, 'camera-zoom-out');
+        btn.onclick = function() {
+            _this.zoom(1);
+        };
+
+        // x+
+        var btn = this.addButton(wrapper, 'camera-x-possitive');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(1.5, 0.0, 0.0));
+        };
+
+        // x-
+        var btn = this.addButton(wrapper, 'camera-x-negative');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(-1.5, 0.0, 0.0));
+        };
+
+        // y+
+        var btn = this.addButton(wrapper, 'camera-y-possitive');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(0.0, 1.5, 0.0));
+        };
+
+        // y-
+        var btn = this.addButton(wrapper, 'camera-y-negative');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(0.0, -1.5, 0.0));
+        };
+
+        // z+
+        var btn = this.addButton(wrapper, 'camera-z-possitive');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(0.0, 0.0, 1.5));
+        };
+
+        // z-
+        var btn = this.addButton(wrapper, 'camera-z-negative');
+        btn.onclick = function() {
+            _this.startTransition(this.position, new Vector(0.0, 0.0, -1.5));
+        };
+
+    }
+
+    addButton(parent, className) {
+        var btn = document.createElement('div');
+        btn.className = 'camera-button ' + className;
+        parent.appendChild(btn);
+
+        return btn;
+    }
+
+    startTransition(from, to) {
+        var time = 0;
+        var timer = setTimeout(function(){
+            this.position = lerp(from, to, time);
+            this.updateViewMatrix();
+
+            time += 40 / 2000; // the whole transition takes 2 seconds
+
+            if(time >= 1) {
+                clearTimeout(timer);
+            }
+        }, 40);
+    }
+
+    lerp(from, to, time) {
+        return new Vector((to.x - from.x) * time + from.x,
+                          (to.y - from.y) * time + from.y,
+                          (to.z - from.z) * time + from.z);        
+    }
 }
