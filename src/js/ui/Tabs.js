5dd8beb62be9d5277760e2c4b8d932d19954988a
Žiga Lesar <terier.kuza@gmail.com>
2020-09-09 15:54:12 +0200

    Default panel

--- src/js/ui/Tabs.js
+++ src/js/ui/Tabs.js
@@ -10,7 +10,7 @@ constructor(options) {
     this._handleClick = this._handleClick.bind(this);
 
     this._tabs = [];
-    this._index = 0;
+    this._index = options.index || 0;
 }
 
 add(name, object) {
